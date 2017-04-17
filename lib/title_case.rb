b00b2ee05216efdfb6fdf3e575706c93ce4e5ba8
Max Reichman and Peter Karellas <peter@>
2017-04-17 09:36:34 -0700

    Finish the project.

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,10 +1,14 @@
 class String
   define_method (:title_case) do
-    word_array = self.split(" ")
+    word_array = self.split()
     empty_array = []
     word_array.each() do |word|
-      empty_array.push(word.capitalize())
+      if word.length > 2
+        empty_array.push(word.capitalize())
+      else
+        empty_array.push(word)
+      end
     end
-  empty_array.join()
+  empty_array.join(" ")
   end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,4 +5,7 @@ describe('String#title_case') do
   it("When a string with one word is entered, the first letter of that word is capitalized") do
     expect(("peter").title_case()).to(eq("Peter"))
   end
+  it ("Any 2-letter words will not be capitalized") do
+    expect(("peter griffin is fat").title_case()).to(eq("Peter Griffin is Fat"))
+  end
 end
